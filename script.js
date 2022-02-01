962b1a1b94877dace8481d5efec930b6abdf85d3
Himynameismarco <49675532+Himynameismarco@users.noreply.github.com>
2022-02-01 09:58:51 +0200

    added comments

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// third solution - with the help of Mr. Anton - with .querySelectorAll we create an Array 
+// of all elements of the class '.clickable'. 
+// for each of these we add an EventListener 
+// and !! for each event added !!, we apply the scrollToFunction
+
 document.querySelectorAll('.clickable').forEach( scrollFromElement => {
     scrollFromElement.addEventListener('click', event => {scrollToFunction(event)});
   });
@@ -8,6 +13,8 @@ function scrollToFunction(event) {
     console.log(scrollToElement);
     scrollToElement.scrollIntoView({ behavior: 'smooth'});
 }
+
+// second solution used replace() and variables to make the function more general 
 
 /*
 const obenEinsElement = document.getElementById("1-oben");
@@ -30,6 +37,8 @@ function scrollFunction(event) {
     scrollToElement.scrollIntoView({ behavior: 'smooth'});
 }
 */
+
+// First solution: a function and a const for each element
 
 /*
 const obenEinsElement = document.getElementById("1-oben");
